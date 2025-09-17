91dbe93dcd3cd318b8ac3f86bc7a042985d424c8
Saravjit Singh <singh.sarav@northeastern.edu>
2025-09-17 15:09:10 -0400

    Added required content on the module side

--- app/(Kambaz)/Courses/[cid]/Modules/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Modules/page.tsx
@@ -1,30 +1,72 @@
 export default function Modules() {
   return (
     <div>
-      {/* Implement Collapse All button, View Progress button, etc. */}
       <button>Collapse All</button>
       <button>View Progress</button>
       <button>Publish All</button>
       <button>Module</button>
       <ul id="wd-modules">
-        <li className="wd-module">
-          <div className="wd-title">Week 1</div>
-          <ul className="wd-lessons">
-            <li className="wd-lesson">
-              <span className="wd-title">LEARNING OBJECTIVES</span>
-              <ul className="wd-content">
-                <li className="wd-content-item">Introduction to the course</li>
-                <li className="wd-content-item">Learn what is Web Development</li>
-              </ul>
-            </li>
-          </ul>
-        </li>
-        <li className="wd-module">
-          <div className="wd-title">Week 2</div>
-        </li>
-        <li className="wd-module">
-          <div className="wd-title">Week 3</div>
-        </li>
+        <li className="wd-module" id="wd-week-1">
+  <div className="wd-title">Week 1</div>
+  <ul className="wd-lessons">
+    <li className="wd-lesson">
+      <span className="wd-title">LEARNING OBJECTIVES</span>
+      <ul className="wd-content">
+        <li className="wd-content-item">Introduction to the course</li>
+        <li className="wd-content-item">Learn what is Web Development</li>
+      </ul>
+    </li>
+    <li className="wd-lesson">
+      <span className="wd-title">READING</span>
+      <ul className="wd-content">
+        <li className="wd-content-item">Full Stack Developer - Chapter 1 - Introduction</li>
+        <li className="wd-content-item">Full Stack Developer - Chapter 2 - Creating User Interfaces</li>
+      </ul>
+    </li>
+    <li className="wd-lesson">
+      <span className="wd-title">SLIDES</span>
+      <ul className="wd-content">
+        <li className="wd-content-item">Introduction to Web Development</li>
+        <li className="wd-content-item">Creating an HTTP server with Node.js</li>
+        <li className="wd-content-item">Creating a React Application</li>
+      </ul>
+    </li>
+  </ul>
+</li>
+
+<li className="wd-module" id="wd-week-2">
+  <div className="wd-title">Week 2</div>
+  <ul className="wd-lessons">
+    <li className="wd-lesson">
+      <span className="wd-title">LEARNING OBJECTIVES</span>
+      <ul className="wd-content">
+        <li className="wd-content-item">Learn how to create user interfaces with HTML</li>
+        <li className="wd-content-item">Deploy the assignment to Netlify</li>
+      </ul>
+    </li>
+    <li className="wd-lesson">
+      <span className="wd-title">SLIDES</span>
+      <ul className="wd-content">
+        <li className="wd-content-item">Introduction to HTML and the DOM</li>
+        <li className="wd-content-item">Formatting Web content with Headings</li>
+        <li className="wd-content-item">Formatting content with Lists and Tables</li>
+      </ul>
+    </li>
+  </ul>
+</li>
+
+<li className="wd-module" id="wd-week-3">
+  <div className="wd-title">Week 3</div>
+  <ul className="wd-lessons">
+    <li className="wd-lesson">
+      <span className="wd-title">LEARNING OBJECTIVES</span>
+      <ul className="wd-content">
+        <li className="wd-content-item">Will be updated later on</li>
+      </ul>
+    </li>
+  </ul>
+</li>
+
       </ul>
     </div>
 );}
--- app/Labs/Lab1/page.tsx
+++ app/Labs/Lab1/page.tsx
@@ -17,7 +17,7 @@ import Image from "next/image";
 export default function Lab1() {
   return (
     <div id="wd-lab1">
-      <h1>Saravjit Singh ------ Section XX</h1>
+      <h1>Saravjit Singh ------ Section 05</h1>
       <h2>Lab 1</h2>
       <h3>HTML Examples</h3>
       <div id="wd-h-tag">
