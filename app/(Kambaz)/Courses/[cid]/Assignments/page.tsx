1226425ab6cb5135d42bc6aa85e6c61e5575e035
Saravjit Singh <singh.sarav@northeastern.edu>
2025-09-17 12:49:24 -0400

    Removed error related to the Link tag

--- app/(Kambaz)/Courses/[cid]/Assignments/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Assignments/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 export default function Assignments() {
   return (
     <div id="wd-assignments">
@@ -9,29 +10,29 @@ export default function Assignments() {
         ASSIGNMENTS 40% of Total <button>+</button> </h3>
       <ul id="wd-assignment-list">
         <li className="wd-assignment-list-item">
-          <link href="/Courses/1234/Assignments/123"
+          <Link href="/Courses/1234/Assignments/123"
              className="wd-assignment-link" >
             A1 - ENV + HTML
-          </link> 
+          </Link> 
           <div className="wd-assignment-details">
     Multiple Modules | <strong>Not available until</strong> May 6 at 12:00am |
     <strong> Due</strong> May 13 at 11:59pm | 100 pts
   </div>
   </li>
         <li className="wd-assignment-list-item">
-          <link href="/Courses/1234/Assignments/234"
+          <Link href="/Courses/1234/Assignments/234"
              className="wd-assignment-link" >
             A2 - CSS + BOOTSTRAP
-          </link>
+          </Link>
           <div className="wd-assignment-details">
     Multiple Modules | <strong>Not available until</strong> May 13 at 12:00am |
     <strong> Due</strong> May 20 at 11:59pm | 100 pts
   </div> </li>
         <li className="wd-assignment-list-item">
-          <link href="/Courses/1234/Assignments/345"
+          <Link href="/Courses/1234/Assignments/345"
              className="wd-assignment-link" >
             A3 - JavaScript + REACT
-          </link>
+          </Link>
           <div className="wd-assignment-details">
     Multiple Modules | <strong>Not available until</strong> May 20 at 12:00am |
     <strong> Due</strong> May 27 at 11:59pm | 100 pts
