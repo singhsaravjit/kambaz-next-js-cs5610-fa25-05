97bf004f9a52c71e4328d4db65e10488481b2744
Saravjit Singh <singh.sarav@northeastern.edu>
2025-09-17 21:15:07 -0400

    deflaut value for sign in page

--- app/(Kambaz)/Account/Signin/page.tsx
+++ app/(Kambaz)/Account/Signin/page.tsx
@@ -3,8 +3,8 @@ export default function Signin() {
     return (
         <div id="wd-signin-screen">
             <h3>Sign in</h3>
-            <input placeholder="username" className="wd-username" /> <br />
-            <input placeholder="password" type="password" className="wd-password" /> <br />
+            <input defaultValue={"saravjit"} placeholder="username" className="wd-username" /> <br />
+            <input defaultValue={"abc123"} placeholder="password" type="password" className="wd-password" /> <br />
             <Link href="/Dashboard" id="wd-signin-btn"> Sign in </Link> <br />
             <Link href="Signup" id="wd-signup-link"> Sign up </Link>
         </div>
