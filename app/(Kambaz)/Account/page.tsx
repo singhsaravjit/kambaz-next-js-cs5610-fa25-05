701e43af2a0819001d5e92aacba62b0c7e71666d
Saravjit Singh <singh.sarav@northeastern.edu>
2025-09-17 20:17:46 -0400

    Some more indentation correction

--- app/(Kambaz)/Account/page.tsx
+++ app/(Kambaz)/Account/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
 export default function Account() {
-   
-        return redirect("/Account/Signin");
-    }
+
+    return redirect("/Account/Signin");
+}
