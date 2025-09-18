cd10baaf678cfb575857260018049145f2a7361c
Saravjit Singh <singh.sarav@northeastern.edu>
2025-09-17 21:09:39 -0400

    added git hub link

--- app/Labs/page.tsx
+++ app/Labs/page.tsx
@@ -17,7 +17,11 @@ export default function Labs() {
           <Link href="/Labs/Lab3" id="wd-lab3-link">
             Lab 3: JavaScript Fundamentals </Link>
         </li>
+        <li>
+          <Link href="/">Kambaz</Link>
+        </li>
       </ul>
+      Github: <Link href="https://github.com/singhsaravjit/kambaz-next-js-cs5610-fa25-05">Click Here</Link> to get the code!
     </div>
   );
 } 
