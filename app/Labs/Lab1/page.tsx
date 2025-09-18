c0d32576ae1f03485f9b196e9f64b55f878eeaf4
Saravjit Singh <singh.sarav@northeastern.edu>
2025-09-17 20:58:51 -0400

    added section

--- app/Labs/Lab1/page.tsx
+++ app/Labs/Lab1/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Lab1() {
   return (
     <div id="wd-lab1">
-      <h1>Saravjit Singh ------ Section 05</h1>
+      <h1>Saravjit Singh ------ Section CS5610.19730.202610</h1>
       <h2>Lab 1</h2>
       <h3>HTML Examples</h3>
       <div id="wd-h-tag">
