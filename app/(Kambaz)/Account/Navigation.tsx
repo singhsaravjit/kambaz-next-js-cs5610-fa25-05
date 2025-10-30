4ac65a97c2ac90dd0b8b7dc3a6866521bd237871
Saravjit Singh <singh.sarav@northeastern.edu>
2025-10-30 00:04:23 -0400

    last changes

--- app/(Kambaz)/Account/Navigation.tsx
+++ app/(Kambaz)/Account/Navigation.tsx
@@ -1,47 +1,31 @@
-'use client';
+"use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-
+import { useSelector } from "react-redux";
 
 export default function AccountNavigation() {
+  const { currentUser } = useSelector((state: any) => state.accountReducer);
+  const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
   const pathname = usePathname();
   
-
-  const isActive = (path: string) => {
-    return pathname?.includes(path);
+  const isActive = (link: string) => {
+    return pathname?.includes(link);
   };
 
   return (
     <div id="wd-account-navigation" className="list-group rounded-0">
-      <Link 
-        href="/Account/Signin" 
-        className={`list-group-item list-group-item-action border-0 ${
-          isActive('Signin') ? 'active border-start border-dark border-4' : 'text-danger'
-        }`}
-        style={isActive('Signin') ? { backgroundColor: 'white', color: 'black' } : {}}
-      >
-        Signin
-      </Link>
-      
-      <Link 
-        href="/Account/Signup" 
-        className={`list-group-item list-group-item-action border-0 ${
-          isActive('Signup') ? 'active border-start border-dark border-4' : 'text-danger'
-        }`}
-        style={isActive('Signup') ? { backgroundColor: 'white', color: 'black' } : {}}
-      >
-        Signup
-      </Link>
-      
-      <Link 
-        href="/Account/Profile" 
-        className={`list-group-item list-group-item-action border-0 ${
-          isActive('Profile') ? 'active border-start border-dark border-4' : 'text-danger'
-        }`}
-        style={isActive('Profile') ? { backgroundColor: 'white', color: 'black' } : {}}
-      >
-        Profile
-      </Link>
+      {links.map((link) => (
+        <Link 
+          key={link}
+          href={`/Account/${link}`}
+          className={`list-group-item list-group-item-action border-0 ${
+            isActive(link) ? 'active border-start border-dark border-4' : 'text-danger'
+          }`}
+          style={isActive(link) ? { backgroundColor: 'white', color: 'black' } : {}}
+        >
+          {link}
+        </Link>
+      ))}
     </div>
   );
 }
